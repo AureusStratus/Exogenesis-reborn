94bf9219ac38e0448e067d98082e5c63e174ccde
SiriusA2 <68311340+AureusStratus@users.noreply.github.com>
2023-07-21 20:13:27 -0400

    Add files via upload

---
+++ src/Exogenesis/graphics/ExoPal.java
@@ -0,0 +1,10 @@
+package Exogenesis.graphics;
+
+import arc.graphics.*;
+import mindustry.graphics.*;
+public class ExoPal {
+public static Color
+ genesisLight = Color.valueOf("e1f2ff"),
+ genesis = Color.valueOf("a9d8ff"),
+ genesisDark = Color.valueOf("66b1ff");
+}
