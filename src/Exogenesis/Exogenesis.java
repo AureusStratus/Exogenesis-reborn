43e71e75e919a29bf1ff3fa7a3696c5dd689d438
SiriusA2 <68311340+AureusStratus@users.noreply.github.com>
2023-07-21 22:22:43 -0400

    Add files via upload

---
+++ src/Exogenesis/Exogenesis.java
@@ -0,0 +1,13 @@
+package Exogenesis.content;
+
+import Exogenesis.content.ExoBlocks;
+import Exogenesis.content.ExoUnitTypes;
+import mindustry.mod.Mod;
+
+public class Exogenesis extends Mod {
+    @Override
+    public void loadContent() {
+        ExoBlocks.load();
+        ExoUnitTypes.load();
+    }
+}
