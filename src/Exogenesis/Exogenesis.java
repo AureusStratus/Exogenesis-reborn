59e479df9f8143d4d7977402e37d3207bf0d4d6e
poly-1810 <105131270+poly-1810@users.noreply.github.com>
2023-07-21 20:59:39 -0500

    h

--- src/Exogenesis/Exogenesis.java
+++ src/Exogenesis/Exogenesis.java
@@ -4,7 +4,7 @@ import Exogenesis.content.ExoBlocks;
 import Exogenesis.content.ExoUnitTypes;
 import mindustry.mod.Mod;
 
-public class ExogenesisReborn extends Mod {
+public class Exogenesis extends Mod {
     @Override
     public void loadContent(){
         ExoBlocks.load();
