e9fca44cef105018d40d9b8234ef59f8bd4dda13
SiriusA2 <68311340+AureusStratus@users.noreply.github.com>
2023-07-23 20:46:53 -0400

    Add files via upload

--- src/Exogenesis/graphics/ExoPal.java
+++ src/Exogenesis/graphics/ExoPal.java
@@ -6,5 +6,9 @@ public class ExoPal {
 public static Color
  genesisLight = Color.valueOf("e1f2ff"),
  genesis = Color.valueOf("a9d8ff"),
- genesisDark = Color.valueOf("66b1ff");
+ genesisDark = Color.valueOf("66b1ff"),
+    empyreanLight = Color.valueOf("fcff98"),
+    empyrean = Color.valueOf("fee761"),
+    empyreanDark = Color.valueOf("f5c045"),
+    empyreanOutline = Color.valueOf("1e1e1e");
 }
