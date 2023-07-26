a3f9893c87086c3d9fb0780c3397fb70e7138789
SiriusA2 <68311340+AureusStratus@users.noreply.github.com>
2023-07-26 00:01:13 -0400

    Add files via upload

--- src/Exogenesis/graphics/ExoPal.java
+++ src/Exogenesis/graphics/ExoPal.java
@@ -4,11 +4,11 @@ import arc.graphics.*;
 import mindustry.graphics.*;
 public class ExoPal {
 public static Color
- genesisLight = Color.valueOf("e1f2ff"),
- genesis = Color.valueOf("a9d8ff"),
- genesisDark = Color.valueOf("66b1ff"),
-    empyreanLight = Color.valueOf("fcff98"),
-    empyrean = Color.valueOf("fee761"),
-    empyreanDark = Color.valueOf("f5c045"),
-    empyreanOutline = Color.valueOf("1e1e1e");
+genesisLight = Color.valueOf("e1f2ff"),
+genesis = Color.valueOf("a9d8ff"),
+genesisDark = Color.valueOf("66b1ff"),
+empyreanLight = Color.valueOf("fcff98"),
+empyrean = Color.valueOf("fee761"),
+empyreanDark = Color.valueOf("f5c045"),
+empyreanOutline = Color.valueOf("1e1e1e");
 }
