53d2682b62435b94abfed6bb27457621db034552
SiriusA2 <68311340+AureusStratus@users.noreply.github.com>
2023-08-04 18:12:36 -0400

    Add files via upload

--- src/Exogenesis/graphics/ExoPal.java
+++ src/Exogenesis/graphics/ExoPal.java
@@ -4,9 +4,12 @@ import arc.graphics.*;
 import mindustry.graphics.*;
 public class ExoPal {
 public static Color
+lightSky = Color.valueOf("#8DB0FF"),
+lightSkyBack = lightSky.cpy().lerp(Color.white, 0.2f),
 genesisLight = Color.valueOf("e1f2ff"),
 genesis = Color.valueOf("a9d8ff"),
 genesisDark = Color.valueOf("66b1ff"),
+genesisDarkAlpha = Color.valueOf("66b1ff90"),
 empyreanLight = Color.valueOf("fcff98"),
 empyrean = Color.valueOf("fee761"),
 empyreanDark = Color.valueOf("f5c045"),
