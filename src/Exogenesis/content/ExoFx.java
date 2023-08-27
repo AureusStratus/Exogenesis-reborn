42e7079cab3a697ba629b1d54e0029344435844e
SiriusA2 <68311340+AureusStratus@users.noreply.github.com>
2023-08-27 18:42:22 -0400

    Add files via upload

--- src/Exogenesis/content/ExoFx.java
+++ src/Exogenesis/content/ExoFx.java
@@ -263,10 +263,12 @@ public class ExoFx{
                 Angles.randLenVectors(e.id, 6, 2.0F + 19.0F * e.finpow(), (x, y) -> Fill.circle(e.x + x / 2.0F, e.y + y / 2.0F, e.fout() * 2f));
                 e.scaled(25f, i -> Angles.randLenVectors(e.id, 6, 2.0F + 19.0F * i.finpow(), (x, y) -> Fill.circle(e.x + x, e.y + y, i.fout() * 4.0F)));
             }),
-            square45_6_45 = new Effect(45f, e-> {
-                Draw.color(e.color, Color.white, e.fout() * 0.6f);
-                randLenVectors(e.id, 7, 25f * e.finpow(), e.rotation, 50f, (x, y) -> {
-                    lineAngle(e.x + x, e.y + y, Mathf.angle(x, y), e.fin() * 5f + 2f);
+            square45_6_45 = new Effect(35f, e-> {
+                color(Color.white, e.color, e.fin());
+                stroke(e.fout() * 1.2f + 0.5f);
+
+                randLenVectors(e.id, 7, 25f * e.finpow(), e.rotation, 60f, (x, y) -> {
+                    lineAngle(e.x + x, e.y + y, Mathf.angle(x, y), e.fin() * 9f + 2f);
                 });
             }),
             blastcolor = new Effect(40f, 600,e -> {
