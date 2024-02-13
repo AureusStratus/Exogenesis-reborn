8c923bc9e809c917cf9395d98142be2f79c1f548
SiriusA2 <68311340+AureusStratus@users.noreply.github.com>
2024-02-13 12:42:18 -0400

    meep

--- src/Exogenesis/content/ExoFx.java
+++ src/Exogenesis/content/ExoFx.java
@@ -75,9 +75,9 @@ public class ExoFx{
                 Draw.color(ExoPal.prometheusColor,e.fout());
                 Lines.stroke(6f*e.fout());
                 Tmp.v1.trns(e.rotation, e.fin()*30f);
-                Lines.ellipse(Tmp.v1.x + e.x, 25 + Tmp.v1.y , 3f * e.fin()+0.1f,6,10, e.rotation);
+                Lines.ellipse(Tmp.v1.x + e.x, Tmp.v1.y + e.y , 3f * e.fin()+0.1f,6,10, e.rotation);
                 Tmp.v1.trns(e.rotation, e.fin()*20f);
-                Lines.ellipse(Tmp.v1.x + e.x, 15 + Tmp.v1.y , 5f * e.fin()+0.1f,8,12, e.rotation);
+                Lines.ellipse(Tmp.v1.x + e.x, Tmp.v1.y + e.y , 5f * e.fin()+0.1f,8,12, e.rotation);
                 Tmp.v2.trns(e.rotation, e.fin()*10f);
                 Lines.ellipse(Tmp.v2.x + e.x, Tmp.v2.y + e.y , 7f*e.fin()+0.1f,10, 18,  e.rotation);
             }),
