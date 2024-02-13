e8a7151626d2d3c786e02db2f5e185be5fad2ffa
SiriusA2 <68311340+AureusStratus@users.noreply.github.com>
2024-02-13 12:28:21 -0400

    meep

--- src/Exogenesis/content/ExoFx.java
+++ src/Exogenesis/content/ExoFx.java
@@ -73,13 +73,13 @@ public class ExoFx{
             PrometheusBeamShockWave = new Effect(60f, 600f, e -> {
                 Draw.z(Layer.effect);
                 Draw.color(ExoPal.prometheusColor,e.fout());
+                Lines.stroke(6f*e.fout());
                 Tmp.v1.trns(e.rotation, e.fin()*30f);
-                Lines.ellipse(Tmp.v1.x + e.x, Tmp.v1.y + e.y , 3f * e.fin()+0.1f,6,10, e.rotation);
+                Lines.ellipse(Tmp.v1.x + e.x, Tmp.v1.y + 25 , 3f * e.fin()+0.1f,6,10, e.rotation);
                 Tmp.v1.trns(e.rotation, e.fin()*20f);
-                Lines.ellipse(Tmp.v1.x + e.x, Tmp.v1.y + e.y , 5f * e.fin()+0.1f,8,12, e.rotation);
+                Lines.ellipse(Tmp.v1.x + e.x, Tmp.v1.y + 15 , 5f * e.fin()+0.1f,8,12, e.rotation);
                 Tmp.v2.trns(e.rotation, e.fin()*10f);
-                Lines.ellipse(Tmp.v2.x + e.x, Tmp.v2.y + e.y , 8f*e.fin()+0.1f,10, 18,  e.rotation);
-                Lines.stroke(7f*e.fout());
+                Lines.ellipse(Tmp.v2.x + e.x, Tmp.v2.y + e.y , 7f*e.fin()+0.1f,10, 18,  e.rotation);
             }),
 
             ullrChargeEffect = new Effect(40f, e -> {
