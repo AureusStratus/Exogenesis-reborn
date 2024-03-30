411ad2b9c8526131cb1bfcbf69b0fef0d8260f0b
SiriusA2 <68311340+AureusStratus@users.noreply.github.com>
2024-03-30 16:35:53 -0400

    meep

--- src/Exogenesis/content/ExoPlanets.java
+++ src/Exogenesis/content/ExoPlanets.java
@@ -1,5 +1,7 @@
 package Exogenesis.content;
 import Exogenesis.graphics.ExoPal;
+import Exogenesis.maps.planets.AxinPlanetGenerator;
+import Exogenesis.maps.planets.TauTiamasPlanetGenerator;
 import arc.graphics.Color;
 import arc.math.Mathf;
 import arc.math.Rand;
@@ -49,10 +51,10 @@ public class ExoPlanets{
         hadroxa = new Planet("hadroxa", Planets.sun, 1f, 2){{
             generator = new ErekirPlanetGenerator();
             meshLoader = () -> new HexMesh(this, 5);
-            cloudMeshLoader = () -> new MultiMesh(
-                    new HexSkyMesh(this, 2, 0.15f, 0.14f, 5, Color.valueOf("eba768").a(0.75f), 2, 0.42f, 1f, 0.43f),
-                    new HexSkyMesh(this, 3, 0.6f, 0.15f, 5, Color.valueOf("eea293").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
-            );
+        //    cloudMeshLoader = () -> new MultiMesh(
+        //            new HexSkyMesh(this, 2, 0.15f, 0.14f, 5, Color.valueOf("eba768").a(0.75f), 2, 0.42f, 1f, 0.43f),
+        //            new HexSkyMesh(this, 3, 0.6f, 0.15f, 5, Color.valueOf("eea293").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
+        //    );
             alwaysUnlocked = true;
             landCloudColor = Color.valueOf("ed6542");
             atmosphereColor = Color.valueOf("f07218");
@@ -62,19 +64,19 @@ public class ExoPlanets{
             atmosphereRadOut = 0.3f;
             tidalLock = true;
             orbitSpacing = 1.2f;
-            totalRadius += 2.6f;
+            totalRadius = 1.9f;
             lightSrcTo = 0.5f;
             lightDstFrom = 0.2f;
             clearSectorOnLose = true;
             defaultCore = Blocks.coreBastion;
             iconColor = Color.valueOf("ff9266");
-            hiddenItems.addAll(Items.serpuloItems).removeAll(Items.erekirItems);
+            hiddenItems.addAll (Items.serpuloItems).removeAll(Items.erekirItems);
             enemyBuildSpeedMultiplier = 0.4f;
 
             allowLaunchToNumbered = false;
             updateLighting = false;
 
-            defaultAttributes.set(Attribute.heat, 0.8f);
+            defaultAttributes.set(Attribute.heat, 1f);
 
             ruleSetter = r -> {
                 r.waveTeam = Team.malis;
@@ -90,17 +92,18 @@ public class ExoPlanets{
             unlockedOnLand.add(Blocks.coreBastion);
         }};
         tauTiamas = new Planet("tauTiamas", Planets.sun, 1f ,2){{
-            generator = new TantrosPlanetGenerator();
+            generator = new TauTiamasPlanetGenerator();
             meshLoader = () -> new HexMesh(this, 4);
             accessible = true;
             visible = true;
-            cloudMeshLoader = () -> new MultiMesh(
-                    new HexSkyMesh(this, 11, 0.25f, 0.13f, 5, new Color().set(ExoPal.genesisLight).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
-                    new HexSkyMesh(this, 1, 0.8f, 0.16f, 6, Color.white.cpy().lerp(ExoPal.genesisLight, 0.55f).a(0.75f), 2, 0.45f, 1.1f, 0.41f)
-            );
+        //    cloudMeshLoader = () -> new MultiMesh(
+        //            new HexSkyMesh(this, 11, 0.25f, 0.13f, 5, new Color().set(ExoPal.genesisLight).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
+        //            new HexSkyMesh(this, 1, 0.8f, 0.16f, 6, Color.white.cpy().lerp(ExoPal.genesisLight, 0.55f).a(0.75f), 2, 0.45f, 1.1f, 0.41f)
+        //    );
             atmosphereColor = Color.valueOf("3db899");
             iconColor = Color.valueOf("597be3");
             startSector = 10;
+            totalRadius = 3.9f;
             atmosphereRadIn = -0.01f;
             atmosphereRadOut = 0.3f;
             defaultEnv = Env.underwater | Env.terrestrial;
@@ -108,17 +111,50 @@ public class ExoPlanets{
 
             };
         }};
-
         vanstar = new Planet("vanstar", Planets.sun, 1f, 3){{
             generator = new SerpuloPlanetGenerator();
             meshLoader = () -> new HexMesh(this, 6);
-            cloudMeshLoader = () -> new MultiMesh(
-                    new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Pal.spore).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
-                    new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Pal.spore, 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
-            );
+//            cloudMeshLoader = () -> new MultiMesh(
+//                    new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Pal.spore).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
+//                    new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Pal.spore, 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
+//            );
 
             launchCapacityMultiplier = 0.5f;
             sectorSeed = 2;
+            orbitRadius = 2.9f;
+            allowWaves = true;
+            allowWaveSimulation = true;
+            allowSectorInvasion = true;
+            allowLaunchSchematics = true;
+            enemyCoreSpawnReplace = true;
+            allowLaunchLoadout = true;
+            //doesn't play well with configs
+            prebuildBase = false;
+            ruleSetter = r -> {
+                r.waveTeam = Team.crux;
+                r.placeRangeCheck = false;
+                r.showSpawns = false;
+            };
+            iconColor = Color.valueOf("7d4dff");
+            atmosphereColor = Color.valueOf("3c1b8f");
+            atmosphereRadIn = 0.02f;
+            atmosphereRadOut = 0.3f;
+            startSector = 15;
+            alwaysUnlocked = true;
+            landCloudColor = Pal.spore.cpy().a(0.5f);
+            hiddenItems.addAll(Items.erekirItems).removeAll(Items.serpuloItems);
+        }};
+        axin = new Planet("axin", Planets.sun, 1.5f, 2){{
+            generator = new AxinPlanetGenerator();
+            meshLoader = () -> new HexMesh(this, 6);
+//            cloudMeshLoader = () -> new MultiMesh(
+//                    new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Pal.spore).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
+//                    new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Pal.spore, 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
+//            );
+
+            launchCapacityMultiplier = 0.5f;
+            sectorSeed = 2;
+            orbitRadius = 8.9f;
             allowWaves = true;
             allowWaveSimulation = true;
             allowSectorInvasion = true;
