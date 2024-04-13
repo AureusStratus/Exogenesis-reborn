3847a1a12111483ae9c5f9b5ced36032da480e16
SiriusA2 <68311340+AureusStratus@users.noreply.github.com>
2024-04-13 18:39:30 -0400

    meep

---
+++ src/Exogenesis/world/meta/ExoEnv.java
@@ -0,0 +1,10 @@
+package Exogenesis.world.meta;
+
+
+/** Environmental flags for different types of locations. */
+public class ExoEnv {
+    public static final int
+    radioactive = 1 << 8,
+    blazing = 1 << 9,
+    freezing = 1 << 10;
+}
